879c4321f36ad846b3b83b86eb82331421e4bebd
quentin <quentin@wiredcraft.com>
2011-04-06 19:58:36 +0800

    First commit

---
+++ themes/kenver/template.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Preprocess functions ===============================================
+ */
+
+/**
+ * Implementation of preprocess_page().
+ */
+function sail_preprocess_page(&$vars) {
+}
+
+/**
+ * Implementation of preprocess_block().
+ */
+function enviso_preprocess_block(&$vars) {
+  // var_dump($vars['block']);
+}
+
+/**
+ * Implementation of preprocess_node().
+ */
+function sail_preprocess_node(&$vars) {
+}
+
+/**
+ * Function overrides =================================================
+ */
