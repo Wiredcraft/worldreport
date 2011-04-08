912f0332409a4e42fa22fb7f9b3402b652dec963
Jackey <jackey@wiredcraft.com>
2011-04-08 14:00:10 +0800

    add news block

--- themes/kenver/template.php
+++ themes/kenver/template.php
@@ -7,22 +7,42 @@
 /**
  * Implementation of preprocess_page().
  */
-function sail_preprocess_page(&$vars) {
+function kenver_preprocess_page(&$vars) {
 }
 
 /**
  * Implementation of preprocess_block().
  */
-function enviso_preprocess_block(&$vars) {
+function kenver_preprocess_block(&$vars) {
   // var_dump($vars['block']);
 }
 
 /**
  * Implementation of preprocess_node().
  */
-function sail_preprocess_node(&$vars) {
+function kenver_preprocess_node(&$vars) {
 }
 
 /**
- * Function overrides =================================================
+ * Implementation hook_preprocess_views_view
  */
+function kenver_preprocess_views_view(&$vars) {
+  $view = $vars['view'];
+  
+  if ($view->name == 'news') {
+    // for frontpage first news block
+    if ($view->current_display == 'block_4') {
+      
+    }
+    // for attach
+    if (in_array($view->current_display, array('attachment_1', 'attachment_2', 'attachment_3')) ) {
+       $term_filter = $view->filter['tid'];
+       foreach ($term_filter->value as $tid) {
+         $term = taxonomy_get_term($tid);
+         $output .= '<span class="term-'.$tid.'">'.l($term->name, taxonomy_term_path($term)).'<span>';
+       }
+       $vars['header'] = '<div class="header header-'.$view->name.' header-"'.$view->name.'-'.$view->current_display.'>';
+       $vars['header'] .= $output.'</div>';
+    }
+  }
+} 
